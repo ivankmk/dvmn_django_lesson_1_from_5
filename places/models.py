54fd73d1ae5ef2560dba70448b2eb869435a1169
IvanKMK <kumeyko@hotmail.com>
2021-01-10 14:13:11 +0100

    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -10,3 +10,11 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+class Image(models.Model):
+    image = models.ImageField(verbose_name='Картинка')
+    place = models.ForeignKey('Place', on_delete=models.CASCADE,related_name='images', verbose_name='Место')
+    number = models.PositiveIntegerField(default=0, verbose_name='Позиция картинки')
+
+    def __str__(self):
+        return f'{self.number} - {self.place}'
