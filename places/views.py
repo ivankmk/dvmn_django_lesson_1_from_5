1a7eadeb0107da2d4935bd12abcf2b103a712e36
IvanKMK <kumeyko@hotmail.com>
2021-01-10 17:01:35 +0100

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Place
 
 # Create your views here.
+def get_places(request):
+    places = Place.objects.all()
+    place_data = {"type": "FeatureCollection", "features": []}
+    for place in places:
+        place_data['features'].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.longitude, place.latitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": reverse('get_place', args=[place.id])
+                }
+            }
+        )
+    return render(request, 'index.html', context={'data': place_data})
+
+
+def get_place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    data = {
+        "title": place.title,
+        "imgs": [request.build_absolute_uri(image.image.url) for image in place.images.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+    }
+    return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from where_to_go import views
+from places import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.get_places),
+    path('places/<int:place_id>', views.get_place, name='get_place'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
