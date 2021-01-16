c8d01820ba6557bf867b43bbad339973f19942fe
IvanKMK <kumeyko@hotmail.com>
2021-01-16 17:36:14 +0100

    deploying to pyanywhere

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
 
 SECRET_KEY = env.str('SECRET_KEY', default='TO_CHANGE')
 
