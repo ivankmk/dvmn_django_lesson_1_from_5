763b75317700aee8ebcb296f53d9bceeacc7732b
IvanKMK <kumeyko@hotmail.com>
2021-01-16 23:35:16 +0100

    static fixing

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -34,7 +34,7 @@ DEBUG = env.bool('DEBUG', default=True)
 SECRET_KEY = env.str('SECRET_KEY', default='TO_CHANGE')
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
