17f6abdaccad806bfd7d46797e9e5ffbe0459b4c
IvanKMK <kumeyko@hotmail.com>
2021-01-16 17:51:34 +0100

    deploying to pyanywhere - p3

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 from environs import Env
 from dotenv import load_dotenv
 
@@ -21,7 +21,7 @@ env = Env()
 env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
