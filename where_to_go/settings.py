8be01b98f510f0395501360f1260ac3832e8f57e
IvanKMK <kumeyko@hotmail.com>
2021-01-19 23:04:14 +0100

    still trying to fix static

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = env.str('STATIC_DIR', default='collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'collected_static'),
-    os.path.join(BASE_DIR, 'static'),
-    ]
+    os.path.join(BASE_DIR, 'assets'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
