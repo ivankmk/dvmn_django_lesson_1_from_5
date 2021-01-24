16ebbb013a0fdbddcb5d92ae2c0b1dca9f9334fa
IvanKMK <kumeyko@hotmail.com>
2021-01-24 17:28:40 +0100

    added management command

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,55 @@
+import requests
+from io import BytesIO
+import os
+from urllib.parse import urlparse
+from django.core.management.base import BaseCommand
+from places.models import Image, Place
+
+
+class Command(BaseCommand):
+    help = 'Adding the additional places into the database.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', type=str, help='URL for .json file.')
+
+    def load_place(self, place_details):
+        title = place_details.get('title', '')
+        latitude = place_details.get('coordinates', {}).get('lat', 0)
+        longitude = place_details.get('coordinates', {}).get('lng', 0)
+        short_description = place_details.get('description_short', '')
+        long_description = place_details.get('description_long', '')
+
+        place_to_load = {
+            'title': title,
+            'latitude': latitude,
+            'longitude': longitude,
+            'short_description': short_description,
+            'long_description': long_description
+        }
+
+        place, created = Place.objects.get_or_create(
+            title=title,
+            defaults=place_to_load
+            )
+        print(f'Place saved: {title}')
+        return place
+
+    def load_place_images(self, image_urls, place):
+        for image_url in image_urls:
+            image = Image.objects.create(place=place)
+            response = requests.get(image_url)
+            response.raise_for_status()
+            image_content = response.content
+            image.image.save(
+                os.path.basename(urlparse(image_url).path),
+                BytesIO(image_content)
+                )
+            print(f'Image saved: {image_url}')
+
+    def handle(self, *args, **options):
+        response = requests.get(options['url'])
+        response.raise_for_status()
+        place_details = response.json()
+        place = self.load_place(place_details)
+        image_urls = place_details.get('imgs', [])
+        self.load_place_images(image_urls, place)
--- places/models.py
+++ places/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from tinymce.models import HTMLField
 
-# Create your models here.
+
 class Place(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название')
-    short_description = models.TextField(verbose_name='Короткое описание', blank=True)
+    short_description = models.TextField(
+        verbose_name='Короткое описание',
+        blank=True)
     long_description = HTMLField(verbose_name='Полное описание', blank=True)
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
@@ -12,10 +14,19 @@ class Place(models.Model):
     def __str__(self):
         return self.title
 
+
 class Image(models.Model):
     image = models.ImageField(verbose_name='Картинка')
-    place = models.ForeignKey('Place', on_delete=models.CASCADE,related_name='images', verbose_name='Место')
-    image_position = models.PositiveIntegerField(default=0, blank=False, null=False, verbose_name='Позиция картинки')
+    place = models.ForeignKey(
+        'Place',
+        on_delete=models.CASCADE,
+        related_name='images',
+        verbose_name='Место')
+    image_position = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+        verbose_name='Позиция картинки')
 
     class Meta(object):
         ordering = ['image_position']
